1e8adf0a8458bbac7251ab56d47498fa8fe14d6b
Leu Family <romanianfobjr@gmail.com>
2021-06-14 16:38:44 -0500

    task 5 progress, add total scores and final message

--- index.js
+++ index.js
@@ -101,10 +101,10 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(inningScoreCB) {
+function getInningScore(inningCB) {
   return{
-    Home: inningScoreCB(),
-    Away: inningScoreCB()
+    Home: inningCB(),
+    Away: inningCB()
   }
 }
 
@@ -150,9 +150,19 @@ Use the scoreboard function below to do the following:
 ]  
   */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(inningScoreCB, inningCB, innings) {
+  let totalGame = [];
+  let homeScore = 0;
+  let awayScore = 0;
+  for (let i = 0; i < innings; i++){
+    const currentScore = inningScoreCB(inningCB);
+    homeScore = homeScore + currentScore.Home,
+    awayScore = awayScore + currentScore.Away
+    totalGame.push(`Inning ${i+1}: Away: ${currentScore.Away} - Home: ${currentScore.Home}`);
+  }
+  return totalGame;
 }
+console.log(scoreboard(getInningScore, inning, 9));
 
 
 
